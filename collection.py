2793d377c3835dc326e28edec8da6fb7f361dddb
natai <satzin0521@gmail.com>
2023-08-11 13:59:09 +0900

    variable and collection

---
+++ collection.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+
+def main():
+    x = [0, 1, 2, 3, 4]
+    y = (10, 3.14, 'Hello, World!')
+    a, b, c = y
+    dic = { 'name': 'John Doe', 'age': 30 }
+    z = {0, 1, 2, 2, 3, 4, 4}
+
+
+if __name__ == '__main__':
+    main()
---
+++ variable.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+
+def main():
+    x = 10
+    y = 3.14
+    total = 5 + 10
+    difference = 95.5 - 4.3
+    product = 4 * 30
+    division = 56.7 / 32.2
+    quotient = 62 // 12
+    remainder = 43 % 5
+    t = True
+    f = False
+    h = 'Hello, World!'
+    j = '日本語'
+    s1 = f"The value of x is {x}"
+    s2 = f"The value of x * x is {x * x}"
+    b = b"0xDEADBEEF"
+    n = None
+    PI = 3.14
+
+
+if __name__ == '__main__':
+    main()
